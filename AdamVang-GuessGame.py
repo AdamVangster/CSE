727b8384760e621cbec380d43d8bdef69ba9b8a4
Adam Vang <31043545+AdamVangster@users.noreply.github.com>
2017-12-04 09:46:42 -0800

    Fixed The INPUT
    12/4/17

---
+++ AdamVang-GuessGame.py
@@ -0,0 +1,21 @@
+import random
+print("Welcome to the Guessing Game!!")
+print("The computer will pick a number from 1 to 50 and")
+print("you will have to guess what it is.")
+print("Ready")
+print("Set")
+print("Start")
+
+# REMEMBER, INPUTS FROM USERS ARE ALWAYS (!!!)
+# OF TYPE STRING!!!!
+
+print(random.randint(1, 50))
+
+# user_guess = correct_answer
+
+# def correct_answer(user_guess):
+#     if user_guess == input("correct_answer"):
+#         print("Higher")
+#     if user_guess == input("correct_answer"):
+#         print("Lower")
+
---
+++ Warmups.py
@@ -0,0 +1,4 @@
+def reverse_order(first_name, last_name):
+    # print("%s. %s" % (last_name, first_name))
+    print(last_name + " " + first_name)  # Concatenation
+
