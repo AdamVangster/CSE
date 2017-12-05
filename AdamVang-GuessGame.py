6da755d680205e97236edb4937f7033398f7bba3
Adam Vang <31043545+AdamVangster@users.noreply.github.com>
2017-12-05 09:44:40 -0800

    Added New Codes
    12/5/17

--- AdamVang-GuessGame.py
+++ AdamVang-GuessGame.py
@@ -1,21 +1,33 @@
 import random
+
 print("Welcome to the Guessing Game!!")
 print("The computer will pick a number from 1 to 50 and")
 print("you will have to guess what it is.")
 print("Ready")
 print("Set")
-print("Start")
+print("Guess")
 
 # REMEMBER, INPUTS FROM USERS ARE ALWAYS (!!!)
 # OF TYPE STRING!!!!
 
-print(random.randint(1, 50))
+# 1. Generate a number
+# 2. Ask the user for an input(number)
+# 3. Does the guess match the number
+# 4. Add "Higher" amd "Lower"
+# 5. Add 5 guess
 
-# user_guess = correct_answer
 
-# def correct_answer(user_guess):
-#     if user_guess == input("correct_answer"):
-#         print("Higher")
-#     if user_guess == input("correct_answer"):
-#         print("Lower")
+correct_answer = (random.randint(1, 3))
+
+user_guess = correct_answer
+
+user_guess(input(" "))
+
+def user_guess(correct_answer):
+    user_guess(input(" "))
+    if correct_answer >= user_guess:
+        print("Lower")
+    elif correct_answer <= user_guess:
+        print("Higher")
+
 
--- Warmups.py
+++ Warmups.py
@@ -2,3 +2,10 @@ def reverse_order(first_name, last_name):
     # print("%s. %s" % (last_name, first_name))
     print(last_name + " " + first_name)  # Concatenation
 
+
+# 12.5.17
+
+def add_py(name):
+    print("%s.py" % name)
+    print(name + ".py")
+
