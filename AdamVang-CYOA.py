7feccf22f43668b48d1b3e4a19d483389e605712
Adam Vang <31043545+AdamVangster@users.noreply.github.com>
2018-04-04 15:30:56 -0700

    4/4/18

--- AdamVang-CYOA.py
+++ AdamVang-CYOA.py
@@ -1,21 +1,24 @@
 import time
+time_delay_short = .2  # default is 2
+time_delay_long = .3  # Default is 3
+
 Instruction = {
     print('Welcome to Escape The House'),
-    time.sleep(2),
+    time.sleep(time_delay_short),
     print("You was trick into a haunted house by bullies."),
     print("There is a ghost in the house that will get closer"),
     print("to you each time you make a move."),
-    time.sleep(3),
+    time.sleep(time_delay_long),
     print("There are multiple items to find and puzzle to solve to"),
     print("get the key to open the lock in the front door"),
-    time.sleep(3),
+    time.sleep(time_delay_long),
     print("There are lockers all around where you can"),
     print("hide in so you won't get caught by the ghost."),
-    time.sleep(3),
+    time.sleep(time_delay_long),
     print("When the ghost is close to you, you have a"),
     print("weird sense of feeling in the air."),
     print("At that moment, you should HIDE in one of the locker."),
-    time.sleep(2),
+    time.sleep(time_delay_short),
     print("Good Luck")
 }
 
@@ -72,44 +75,41 @@ class Locker(Interactable):
         Interactable.hide(self.name)
 
 
-class Lock(Interactable):
+class PadLock(Interactable):
     def __init__(self, name, description):
-        super(Lock, self).__init__(name, description)
+        super(PadLock, self).__init__(name, description)
 
     def open(self):
         Interactable.open(self.name)
 
 
 class Safe(Interactable):
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         super(Safe, self).__init__(name, description)
+        self.items = items
 
     def open(self):
         Interactable.open(self.name)
-
-
-class Painting(Interactable):
-    def __init__(self, name, description):
-        super(Painting, self).__init__(name, description)
-
-    def move(self):
-        Interactable.move(self.name)
 
 
 class Drawer(Interactable):
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         super(Drawer, self).__init__(name, description)
+        self.items = items
 
     def open(self):
         Interactable.open(self.name)
 
-
-class CrackedWall(Interactable):
-    def __init__(self, name, description):
-        super(CrackedWall, self).__init__(name, description)
-
     def smash(self):
         Interactable.smash(self.name)
+
+
+class FrontDoor(Interactable):
+    def __index__(self, name, description):
+        super(FrontDoor, self).__init__(name, description)
+
+    def open(self):
+        Interactable.open(self.name)
 
 
 # Tool
@@ -120,15 +120,6 @@ class Tool(Item, Interactable):
 
     def open(self):
         Interactable.open(self.name)
-
-    def smash(self):
-        Interactable.smash(self.name)
-
-
-class Hammer(Tool):
-    def __init__(self, name, description):
-        Item.__init__(self, name, description)
-        Interactable.__init__(self, name, description)
 
     def smash(self):
         Interactable.smash(self.name)
@@ -151,6 +142,12 @@ class LockKey(Key):
     def open(self):
         Key.open(self.name)
 
+    def pick_up(self):
+        Item.pick_up(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
+
 
 class SafeKey(Key):
     def __init__(self, name, description):
@@ -158,6 +155,33 @@ class SafeKey(Key):
 
     def open(self):
         Key.open(self.name)
+
+    def pick_up(self):
+        Item.pick_up(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
+
+
+class StorageKey(Key):
+    def __init__(self, name, description):
+        super(StorageKey, self).__init__(name, description)
+
+    def open(self):
+        Key.open(self.name)
+
+    def pick_up(self):
+        Item.pick_up(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
+
+    def use(self, player):
+        if player.location.name == "Arcade Room":
+            player.location.south = "storage_rm"
+            print("You unlock the door to the storage room")
+        else:
+            print("The key doesn't seem to work here")
 
 
 # Consumable
@@ -173,6 +197,12 @@ class Pizza(Consumable):
     def __init__(self, name, description):
         super(Pizza, self).__init__(name, description)
 
+    def pick_up(self):
+        Item.put_on(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
+
     def eat(self):
         Item.eat(self.name)
 
@@ -180,6 +210,12 @@ class Pizza(Consumable):
 class WaterBottle(Consumable):
     def __init__(self, name, description):
         super(WaterBottle, self).__init__(name, description)
+
+    def pick_up(self):
+        Item.put_on(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
 
     def drink(self):
         Item.drink(self.name)
@@ -193,10 +229,22 @@ class Wearable(Item):
     def put_on(self):
         Item.put_on(self.name)
 
+    def pick_up(self):
+        Item.put_on(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
+
 
 class Clothes(Wearable):
     def __init__(self, name, description):
         super(Clothes, self).__init__(name, description)
+
+    def pick_up(self):
+        Item.pick_up(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
 
     def put_on(self):
         Item.put_on(self.name)
@@ -206,36 +254,48 @@ class Mask(Wearable):
     def __init__(self, name, description):
         super(Mask, self).__init__(name, description)
 
+    def pick_up(self):
+        Item.pick_up(self.name)
+
+    def drop(self):
+        Item.drop(self.name)
+
     def put_on(self):
         Item.put_on(self.name)
 
 
 # Containers
 class Container(Item):
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         super(Container, self).__init__(name, description)
+        self.items = items
 
     def pick_up(self):
-        Item.put_on(self.name)
+        Item.pick_up(self.name)
 
     def open(self):
         Item.open(self.name)
 
 
 class Box(Container):
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         super(Box, self).__init__(name, description)
+        self.items = items
 
     def pick_up(self):
-        Container.put_on(self.name)
+        Container.pick_up(self.name)
+
+    def drop(self):
+        Container.drop(self.name)
 
     def open(self):
         Container.open(self.name)
 
 
 class Backpack(Container):
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         super(Backpack, self).__init__(name, description)
+        self.items = items
 
     def pick_up(self):
         Container.pick_up(self.name)
@@ -243,13 +303,20 @@ class Backpack(Container):
     def open(self):
         Container.open(self.name)
 
+    def drop(self):
+        Container.drop(self.name)
+
 
 class Jar(Container):
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         super(Jar, self).__init__(name, description)
+        self.items = items
 
     def pick_up(self):
         Container.pick_up(self.name)
+
+    def drop(self):
+        Container.drop(self.name)
 
     def open(self):
         Container.open(self.name)
@@ -299,6 +366,7 @@ class Ghost(NPC):
 class Hero(Character):
     def __init__(self, name, health, inventory, description):
         super(Hero, self).__init__(name, health, inventory, description)
+        self.location = None
 
     def inventory(self):
         print("You open you inventory.")
@@ -306,8 +374,8 @@ class Hero(Character):
     def health(self):
         print("You only have 1 health.")
 
-    def move(self):
-        print("You moved to a different room.")
+    def move(self, direction):
+        self.location = globals()[getattr(self, direction)]
 
 
 # Rooms
@@ -321,15 +389,11 @@ class Room(object):
         self.description = description
         self.item = item
 
-    def move(self, direction):
-        global current_node
-        current_node = globals()[getattr(self, direction)]
-
 
 # Make Item
 lock_key = LockKey("Lock Key", "This key is use to open the lock on the front door.")
 safe_key = SafeKey("Safe Key", "This key is use to open a safe.")
-hammer = Hammer("Hammer", "This hammer looks like it can smash something.")
+storage_key = StorageKey("Storage Key", "You can the lock on the door at the storage key.")
 jar = Jar("Jar", "Looks like you can throw this.")
 backpack = Backpack("Backpack", "You can carry the backpack but it's useless.")
 box = Box("Box", "There's nothing in the box, you can move them.")
@@ -338,11 +402,10 @@ clothes = Clothes("Clothes", "You can wear these clothes to make you looks good.
 water_bottle = WaterBottle("Watter Bottle", "Still looks drinkable.")
 pizza = Pizza("Pizza", "This pizza still looks eatable.")
 locker = Locker("Locker", "You can hide in it.")
-cracked_wall = CrackedWall("A Cracked Wall", "Looks like this wall can be smash.")
 drawer = Drawer("A Desk Drawer", "There could be item in this.")
-painting = Painting("A Painting", "You can move the painting off the wall.")
 safe = Safe("Safe", "You can open this with a safe key.")
-lock = Lock("Lock", "You can open this lock with a lock key.")
+padlock = PadLock("PadLock", "You can open this lock with a lock key.")
+frontdoor = FrontDoor("Front Door", "You can open the door.")
 
 # Make Characters
 hero = Hero("Dashie", 1, False, "You are T H I C C and you don't have aim.")
@@ -355,7 +418,7 @@ kitchen = Room("Kitchen", None, None, "empty_rm1", "empty_rm2", "There's a cabin
 empty_rm2 = Room("Empty Room", None, "kitchen", "living_rm", None, "A empty room with a backpack.")
 living_rm = Room("Living Room", "empty_rm2", "empty_rm1", "hally_way", "arcade_Rm", "There's a tilted picture frame.")
 arcade_rm = Room("Arcade Room", "storage_rm", "living_rm", "empty_rm3", None, "There's a lot of lockers and games.")
-storage_rm = Room("Storage Room", None, None, "arcade_rm", None, "This is a lot of boxes in this room.")
+storage_rm = Room("Storage Room", None, None, "arcade_rm", None, "There's a safe inside the storage room.")
 empty_rm3 = Room("Empty Room", "arcade_rm", None, None, None, "There's is 1 locker in this room.")
 hallway = Room("HallWay", "living_rm", "bedroom1", "bedroom2", None, "There's a lot of scary paints and lockers.")
 bedroom1 = Room("BedRoom", None, "bathroom1", "closet1", "hallway", "There's a bed with a desk.")
@@ -365,14 +428,13 @@ bedroom2 = Room("BedRoom", "hallway", None, "bathroom2", "closet2", "This is a n
 closet2 = Room("Closet", None, "bedroom2", None, None, "This closet have a lot of boxes and clothes.")
 bathroom2 = Room("Bathroom", "bedroom2", None, None, None, "The bathroom have creepy painting.")
 
-
-current_node = entrance
+hero.location = entrance
 directions = ['north', 'south', 'east', 'west']
 short_directions = ['n', 's', 'e', 'w']
 
 while True:
-    print(current_node.name)
-    print(current_node.description)
+    print(hero.location.name)
+    print(hero.location.description)
     command = input('>_').lower().strip()
     if command == 'quit':
         quit(0)
@@ -381,7 +443,7 @@ while True:
         command = directions[pos]
     if command in directions:
         try:
-            current_node.move(command)
+            hero.move(command)
         except KeyError:
             print("You cannot go this way")
     else:
